ce35afa2cc2bba442505e7b2fd08f12251f689eb
Julio Gonzalez <juliojose.gonzalez@gmail.com>
2013-07-12 00:59:56 +0000

    Updated web.js: add fs variable

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('./index.html','utf8')));
+  response.send(buf.toString(fs.readFileSync('./index.html')));
 });
 
 var port = process.env.PORT || 5000;
