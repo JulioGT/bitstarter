92c5ba2d365339fb4088b656a9abd64b611cc378
Julio Gonzalez <juliojose.gonzalez@gmail.com>
2013-07-12 00:52:57 +0000

    Updated web.js: fix application Error

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var buf = require('buf');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync('./index.html')));
+  response.send(buf.toString(fs.readFileSync('./index.html','utf8')));
 });
 
 var port = process.env.PORT || 5000;
